3065cc06bd89ef3966c32c0ea51c98e6a509a392
lucius <aebyl@bzz.ch>
2024-12-05 14:05:41 +0100

    learning react

---
+++ nodejs/src/components/ProductList.jsx
@@ -0,0 +1,15 @@
+import Product from "./Product";
+
+export default function App() {
+    return (
+        <div className="flex flex-wrap w-full p-5 ">
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+        </div>
+    )
+}
