268d8e59a9db64975d9190dde187bf109c19cb37
lucius <aebyl@bzz.ch>
2024-12-05 16:10:28 +0100

    Seiten eingefügt und Produkte limitiert.

--- learning-react/src/components/Product.jsx
+++ learning-react/src/components/Product.jsx
@@ -4,8 +4,8 @@ export default function Product(probs) {
         <div className="w-1/4 p-5">
             <img src={product.thumbnail}/>
             <h2 className="font-bold">{product.title}</h2>
-            <p>{product.description}</p>
-            <p className=" text-gray-500">$ {product.price}</p>
+            <p  className="text-grey-200">{product.description}</p>
+            <p className=" text-black ">$ {product.price}</p>
         </div>
     )
 }
--- learning-react/src/components/ProductList.jsx
+++ learning-react/src/components/ProductList.jsx
@@ -1,22 +1,51 @@
 import Product from "./Product";
 import {useEffect, useState} from "react";
 
+
+
 export default function ProductList() {
+
+    const limit = 12;
+
+
+    const [page, setPage] = useState(1);
+
+
+
+
     const [products, setProducts] = useState([]);
 
+
+
     useEffect( ()  => {
-        fetch("https://dummyjson.com/products")
-            .then(res => res.json())
+        let skip = (page - 1) * limit;
+        fetch(`https://dummyjson.com/products?limit=${limit}&skip=${skip}`)            .then(res => res.json())
             .then(data => {
                 setProducts(data.products);
             })
-    },  []);
+    },  [page]);
 
     return (
-        <div className="flex flex-wrap w-full p-5 ">
+        <div className="flex flex-wrap w-full p-5">
             {products.map((product) => (
-                <Product key={product.id} product={product} />
-            )) }
+                <Product key={product.id} product={product}/>
+            ))}
+
+            <Pagination
+                page={page}
+                setPage={setPage}
+                maxPage={100}
+            />
+
+            <button className="p-3" onClick={() => setPage(page - 1)}>Prev</button>
+            <button className="p-3" onClick={() => setPage(page - 2)}>{page - 2}</button>
+            <button className="p-3" onClick={() => setPage(page - 1)}>{page - 1}</button>
+            <p className="p-3 text-green-500">{page}</p>
+            <button className="p-3" onClick={() => setPage(page + 1)}>{page + 1}</button>
+            <button className="p-3" onClick={() => setPage(page + 2)}>{page + 2}</button>
+            <button className="p-3" onClick={() => setPage(page + 1)}>Next</button>
         </div>
     )
 }
+
+
